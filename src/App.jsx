e8d3a02d6b9a64e66f23c3afe9cf066f084cd08f
AssiaChemlali <chemlali.assia@gmail.com>
2024-12-14 13:22:17 +0100

    add components files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css'
-
+import Navbar from './components/Navbar'
+import Products from './components/Products'
 function App() {
 
 
   return (
-    <>
-    
-    </>
+    < div className='bg-black p-10 container h-screen'>
+      <Navbar/>
+      <Products/>
+    </div>
   )
 }
 
